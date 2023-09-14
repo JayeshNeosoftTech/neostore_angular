500acec1574aa605114663fb73eff6b214f9e8da
unknown <jayesh.bhosale@neosoftmail.com>
2023-09-14 13:15:41 +0530

    added footer code

--- src/app/components/footer-content/footer-content.component.ts
+++ src/app/components/footer-content/footer-content.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer-content.component.scss']
 })
 export class FooterContentComponent {
-
+  catogoties_link =['Popular', 'Chair', 'Table', 'Sofa', 'Cupboard', 'Lamp'];
+  customer_service =['My Account', 'Discount', 'Returns', 'Order History', 'Order Tracking'];
+  furniture_link = ['Living Room Furniture', 'Bar Furniture', 'Bedroom Furniture', 'Kids Furniture', 'Accent Furniture', 'Book Shelves'];
+  usefule_link = ['Promotions', 'Sitemap'];
 }
